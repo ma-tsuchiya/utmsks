e7f2c79174a700cfddee33e214032ca9184dbbbd
Masaaki Tsuchiya <>
2019-04-18 02:18:31 +0900

    Bマシンマニュアル更新

---
+++ Windows_install_BC_1.md
@@ -0,0 +1,2 @@
+## 2. Windows Install[B, C]
+
---
+++ catenate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cat header_A.md post_header.md bios_A.md Windows_install_A_1.md Windows_install_2.md Windows_install_A_3.md Windows_install_4.md > 2019WindowsInstallManualA.md
+cat header_B.md post_header.md bios_B.md Windows_install_BC_1.md Windows_install_2.md Windows_install_BC_3.md Windows_install_4.md > 2019WindowsInstallManualB.md
+cat header_C.md post_header.md bios_C.md Windows_install_BC_1.md Windows_install_2.md Windows_install_BC_3.md Windows_install_4.md > 2019WindowsInstallManualC.md
+
